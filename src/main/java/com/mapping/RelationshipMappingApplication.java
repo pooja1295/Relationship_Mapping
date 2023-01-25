e90726d4eb9d594932d40d1e2a9e1332eadedde2
Pooja Karma <pooja.karma95@gmail.com>
2023-01-25 16:13:12 +0530

    Initial commit

---
+++ src/main/java/com/mapping/RelationshipMappingApplication.java
@@ -0,0 +1,13 @@
+package com.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipMappingApplication.class, args);
+	}
+
+}
